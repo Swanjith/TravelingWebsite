8ccacfe52261dc511f52b31345c451ccbbc29e80
BNsrujan <srujanbn123@gmail.com>
2024-11-15 10:50:47 +0530

    code base setup

---
+++ Frountend-vite/src/App.tsx
@@ -0,0 +1,16 @@
+import Home from "./app/Home"
+
+
+function App() {
+
+  return (
+    <section>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Home />
+    </section>
+  )
+}
+
+export default App
